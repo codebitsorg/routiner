f8ccc249d5210f253052b77cd1730abf1c0dfbfb
Vladimir Medintcev <vladimir.medintcev@yandex.ru>
2025-04-10 11:50:45 +0400

    Refactor routiner code to a single file.

--- routiner.go
+++ routiner.go
@@ -1,7 +1,9 @@
 package routiner
 
 import (
+	"fmt"
 	"log"
+	"runtime/debug"
 	"sync"
 )
 
@@ -14,6 +16,75 @@ type Routiner struct {
 	workers       int
 	activeWorkers int
 	mu            sync.RWMutex
+}
+
+type option func(*Routiner)
+
+// NewRoutiner creates a new Routiner.
+func NewRoutiner(opts ...option) *Routiner {
+	r := &Routiner{
+		input:   make(chan any),
+		output:  make(chan string),
+		wg:      new(sync.WaitGroup),
+		quitJob: make(chan int),
+		workers: 1,
+	}
+
+	r.With(opts...)
+
+	return r
+}
+
+// New is a helper function to create a new Routiner.
+func New(opts ...option) *Routiner {
+	return NewRoutiner(opts...)
+}
+
+// Init is a helper function to create a new Routiner.
+func Init(opts ...option) *Routiner {
+	return NewRoutiner(opts...)
+}
+
+// With is a helper function to set options
+func (r *Routiner) With(opts ...option) {
+	for _, opt := range opts {
+		opt(r)
+	}
+}
+
+// WithWorkers sets the number of workers to n.
+func WithWorkers(n int) option {
+	return func(r *Routiner) {
+		if n < 1 {
+			n = 1
+		}
+
+		r.workers = n
+	}
+}
+
+// WithBufferedInputChannel creates a buffered input channel
+// with size n.
+func WithBufferedInputChannel(n int) option {
+	return func(r *Routiner) {
+		if n < 1 {
+			n = 1
+		}
+
+		r.input = make(chan any, n)
+	}
+}
+
+// WithBufferedOutputChannel creates a buffered output channel
+// with size n.
+func WithBufferedOutputChannel(n int) option {
+	return func(r *Routiner) {
+		if n < 1 {
+			n = 1
+		}
+
+		r.output = make(chan string, n)
+	}
 }
 
 // Run starts the job processes. It first initializes all
@@ -86,4 +157,117 @@ func (r *Routiner) ActiveWorkers() int {
 // It sets the number of active workers to 0.
 func (r *Routiner) Quit() {
 	r.resetActiveWorkers()
+}
+
+// startManager starts the manager process and waits
+// for all the workers to finish.
+func (r *Routiner) startManager(manager func(r *Routiner)) {
+	defer r.recover()()
+
+	r.inputThrough = make(chan any)
+
+	for range r.Workers() {
+		go func() {
+			defer r.recover()()
+
+			r.work(r.inputThrough)
+		}()
+	}
+
+	manager(r)
+
+	// The inputThrough channel must be closed before
+	// the main input channel to avoid deadlocks.
+	close(r.inputThrough)
+
+	r.waitToFinish()
+}
+
+// startWorkers Run the worker's handler. This function increases the
+// number of active workers and holds the worker until the data is
+// available in the input channel.
+//
+// The function will finish only when all workers have been
+// set to the active state.
+//
+// Once the handler is done, the worker is deactivated:
+// the number of active workers and the activeWorkers
+// wait group are decremented.
+func (r *Routiner) startWorkers(worker func(r *Routiner, input any)) {
+	// Wait for all workers to be active.
+	wgAcitveWorkers := &sync.WaitGroup{}
+	wgAcitveWorkers.Add(r.Workers())
+	defer wgAcitveWorkers.Wait()
+
+	// Start the workers.
+	for range r.Workers() {
+		go func() {
+			defer r.recover()()
+
+			r.activateWorker(wgAcitveWorkers)
+
+			for input := range r.input {
+				for message := range input.(chan any) {
+					worker(r, message)
+				}
+				r.deactivateWorker()
+			}
+		}()
+	}
+}
+
+// work initiate the worker process by sending
+// the data to the input channel.
+func (r *Routiner) work(obj any) {
+	r.input <- obj
+}
+
+// waitToFinish waits for all workers to finish.
+func (r *Routiner) waitToFinish() {
+	r.wg.Wait()
+	r.quitJob <- 0
+}
+
+// Activate a worker.
+func (r *Routiner) activateWorker(wgAcitveWorkers *sync.WaitGroup) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.wg.Add(1)
+	r.activeWorkers++
+	wgAcitveWorkers.Done()
+}
+
+// Deactivate a worker.
+func (r *Routiner) deactivateWorker() {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if r.activeWorkers > 0 {
+		r.activeWorkers--
+		r.wg.Done()
+	}
+}
+
+// Reset Active Workers.
+func (r *Routiner) resetActiveWorkers() {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	for i := 0; i < r.activeWorkers; i++ {
+		r.wg.Done()
+	}
+
+	r.activeWorkers = 0
+}
+
+// Recover the application in case of a panic.
+func (r *Routiner) recover() func() {
+	f := func() {
+		if err := recover(); err != nil {
+			log.Print(fmt.Errorf("panic: %s\n%s", err, debug.Stack()))
+		}
+	}
+
+	return f
 }
--- routiner_test.go
+++ routiner_test.go
@@ -11,6 +11,20 @@ import (
 	"github.com/codebitsorg/routiner"
 )
 
+func TestInit_InitializingRoutinerAddsOneWorkerByDefault(t *testing.T) {
+	r := routiner.Init()
+	if r.Workers() != 1 {
+		t.Errorf("Workers should be 1, but got %d", r.Workers())
+	}
+}
+
+func TestInit_CanOptionalySetWorkers(t *testing.T) {
+	r := routiner.Init(routiner.WithWorkers(4))
+	if r.Workers() != 4 {
+		t.Errorf("Workers should be 4, but got %d", r.Workers())
+	}
+}
+
 func TestRun(t *testing.T) {
 	r := routiner.Init(routiner.WithWorkers(10))
 
